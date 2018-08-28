ae064698259097fae8eb4c1d4f7c3323ea868a5a
Serena-Chenzz <34407958+Serena-Chenzz@users.noreply.github.com>
2018-08-28 13:56:36 +1000

    submission

--- final_submission_proj_1A.c
+++ final_submission_proj_1A.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-using namespace std;
+#include <omp.h>
 
 #define MAX 10000
 #define NOT_CONNECTED -1
@@ -8,11 +8,10 @@ int distance[MAX][MAX];
 
 int nodesCount;
 
-int threadNum;
-
 void Initialize(){
-    for (int i=0;i<MAX;++i){
-        for (int j=0;j<MAX;++j){
+    int i,j;
+    for (i=0;i<MAX;++i){
+        for (j=0;j<MAX;++j){
             distance[i][j]=NOT_CONNECTED;
 
         }
@@ -22,15 +21,11 @@ void Initialize(){
 
 int main(int argc, char** argv){
 
-    //Get the number of threads
-    threadNum = omp_get_num_threads();
-
     if(argc!=2){
         printf("The path to the input file is not specified as a parameter.\n");
         return -1;
     }
-
-    FILE *in_file  = fopen(FILE_PATH, "r");
+    FILE *in_file  = fopen(argv[1], "r");
     if (in_file  == NULL)
     {
         printf("Can't open file for reading.\n");
@@ -59,35 +54,18 @@ int main(int argc, char** argv){
     //Floyd-Warshall
     int k;
     for (k=1;k<=nodesCount;++k){
-        int i,j;
-        // If the threadNum is less than 4, we use static parallel for once
-        // If the threadNum is larger than 4,we use collapse (nested parrallel loop)
-        if(threadNum > 4){
-            #pragma omp parallel for private(j) collapse(2) schedule(dynamic)
-            for (i=1;i<=nodesCount;++i){
-                {
-                    for (j=1;j<=nodesCount;++j){
-                        if ((distance[i][k]!=NOT_CONNECTED)&&(distance[k][j]!=NOT_CONNECTED && (distance[i][j]==NOT_CONNECTED || distance[i][k]+distance[k][j]<distance[i][j]))){
-                            distance[i][j]=distance[i][k]+distance[k][j];
-                        }
+        int i;
+        #pragma omp parallel for schedule(dynamic)
+        for (i=1;i<=nodesCount;++i){
+            if (distance[i][k]!=NOT_CONNECTED){
+                int j;
+                for (j=1;j<=nodesCount;++j){
+                    if (distance[k][j]!=NOT_CONNECTED && (distance[i][j]==NOT_CONNECTED || distance[i][k]+distance[k][j]<distance[i][j])){
+                        distance[i][j]=distance[i][k]+distance[k][j];
                     }
                 }
             }
         }
-        else{
-            #pragma omp parallel for private(j) schedule(static)
-            for (i=1;i<=nodesCount;++i){
-                {
-                    for (j=1;j<=nodesCount;++j){
-                        if ((distance[i][k]!=NOT_CONNECTED)&&(distance[k][j]!=NOT_CONNECTED && (distance[i][j]==NOT_CONNECTED || distance[i][k]+distance[k][j]<distance[i][j]))){
-                            distance[i][j]=distance[i][k]+distance[k][j];
-                        }
-                    }
-                }
-            }
-
-        }
-
     }
 
     int diameter=-1;
@@ -106,7 +84,7 @@ int main(int argc, char** argv){
 
     //Output
     timeEnd = omp_get_wtime();
-    printf("%d %.16g\n", diameter,(timeEnd-timeBegin));
+    printf("%d %.16g", diameter,(timeEnd-timeBegin));
 
     return 0;
 
